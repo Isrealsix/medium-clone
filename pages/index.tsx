711b664fd8b6696e439c39ac7bfd90d30c4450bd
Isrealsix's Macbook pro <isrealsix@icloud.com>
2022-01-25 00:50:19 +0700

    completed the header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,24 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black">
+              Medium</span>{" "} is a place to write, read and connect
+          </h1>
+          <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
+        </div>
+        <img className="hidden md:inline-flex h-32 lg:h-full" src="https://t.ly/j44R" alt="" />
+      </div>
     </div>
   )
 }
