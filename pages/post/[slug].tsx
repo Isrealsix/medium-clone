d748e045351c5d8f13d96f3439f7cf58c00795d5
Isrealsix's Macbook pro <isrealsix@icloud.com>
2022-02-07 02:21:51 +0700

    fetched, display and designed the comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -19,7 +19,6 @@ interface IFormInput {
 
 function Post({ post }: Props) {
   const [submitted, setSubmitted] = useState(false);
-
   const {
     register,
     handleSubmit,
@@ -153,6 +152,21 @@ function Post({ post }: Props) {
         />
       </form>
       )}
+
+      {/* Display approved comments */}
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
