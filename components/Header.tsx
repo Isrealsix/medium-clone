92290919617c94601cbb1f177e08078a08a39d73
Isrealsix's Macbook pro <isrealsix@icloud.com>
2022-02-07 02:22:11 +0700

    removed post url from link

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Header() {
   return <header className="flex justify-between p-5 max-w-7xl mx-auto">
     <div className="flex items-center space-x-5">
-      <Link href="/post">
+      <Link href="/">
         <img
         className="w-44 object-contain cursor-pointer"
         src="https://t.ly/86VH" alt="" />
